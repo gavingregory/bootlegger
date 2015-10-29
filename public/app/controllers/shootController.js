d71ca3aa9949dd16b9069ae90a6944e5c0420853
gavin gregory <gavin.ian.gregory@gmail.com>
2015-10-29 16:51:49 +0000

    Shoot view now displays the following statistics: 'camera person of selected clip' and meta is now displayed completely.

--- public/app/controllers/shootController.js
+++ public/app/controllers/shootController.js
@@ -47,16 +47,20 @@ angular.module('bootleggerApp')
 
   init();
 
-  $scope.play = function (url) {
+  $scope.play = function (url, id) {
     var myVideo = document.getElementsByTagName('video')[0];
     myVideo.src = url;
     myVideo.load();
     myVideo.play();
-    console.log('something');
-    // var video = document.getElementById('video');
-    // video.setAttribute('src', url);
-    // video.load();
-    // video.play();
+
+    // find the correct shoot in the array
+    for (var i=0; i < $scope.shoot.length; i++) {
+      if ($scope.shoot[i].id === id) {
+        $scope.meta = $scope.shoot[i];
+        console.log('found at ' +i);
+        break;
+      }
+    }
   };
 
   $scope.doSort = function (colName) {
