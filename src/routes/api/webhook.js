ca82aebb1df906d80b24db6632d6cedcce8e1b43
Gavin Gregory <gavin.ian.gregory@gmail.com>
2016-04-09 23:40:42 -0400

    Webhooks working, however save results as stringified JSON for now. Need to see what fields get returned, and save them correctly.

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -30,13 +30,23 @@ router.post('/', function (req, res) {
   console.log(valid);
 
   // find task with the task ID
-  Task.findById(payload.data.task_id, function (err, data) {
+  Task.findOne({_id: payload.data.task_id }, function (err, task) {
     if (err) console.log('error finding a matching task');
-    else console.log('success finding a matching task');
+    else if (!task) console.log('No task found');
+    else {
+      console.log('success finding a matching task');
+      var row = 0;
+      for ( ; row < task.jobs.length; row++) {
+        if (task.jobs[row].id === payload.data.id) break;
+      }
+      task.jobs[row].judgments.push({result: JSON.stringify(payload.results) });
+      task.save(function (err, data) {
+        if (err) { console.log('error saving the task'); console.log(err); }
+        else console.log('SUCCESSFULLY RETRIEVED A JUDGMENT' + payload.results);
+        res.status(200).send(sha1);   
+      });
+    }
   });
-
-  res.status(200).send(sha1);
-
 });
 
 module.exports = router;
