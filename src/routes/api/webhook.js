ee513a766ad4b54a0b0890ac8231b26a3fbdb573
Gavin Gregory <gavin.ian.gregory@gmail.com>
2016-04-09 22:06:01 -0400

    Webhook now checks a message for validity by generating an SHA1 checksum. Webhook now attempts to retrieve the Task identified in the payload from the database.

--- src/routes/api/webhook.js
+++ src/routes/api/webhook.js
@@ -1,21 +1,41 @@
 var express = require('express')
-  , multer = require('multer')
-  , requestify = require('requestify')
-  , mime = require('mime')
-  , params = require('../../config/params')
   , router = express.Router({mergeParams: true})
+  , params = require('../../config/params')
   , moment = require('moment')
-  , videoHelper = require('../../services/videoHelper')
+  , util = require('util')
   , Task = require('../../models/task').model
-  , crowdflower = require('../../services/crowdflower/Crowdflower')(params.cf_api)
-  , qs = require('querystring')
-  , Job = require('../../services/crowdflower/Job');
+  , crypto = require('crypto');
+
+/**
+ * SHA1 checksum (required by Crowdflower to sign a webhook post)
+ */
+var SHA1Checksum = function (e) {
+  return crypto.createHash('sha1').update(e).digest('hex');
+}
 
 // post crowdflower job results
 router.post('/', function (req, res) {
 
-  console.log(req.body);
-  res.status(200).send('ok');
+  // generate an sha1 checksum of the (payload + api key)
+  var sha1 = SHA1Checksum(req.body.payload + params.cf_api);
+
+  // parse payload
+  var payload = JSON.parse(req.body.payload);
+
+  console.log(util.inspect(payload, { showHidden: true, depth: null }));
+  console.log(sha1);
+
+  // the webhook is valid if the sha1 generated matches the signature of the post
+  var valid = req.body.signature === sha1;
+  console.log(valid);
+
+  // find task with the task ID
+  Task.findById(payload.data.task_id, function (err, data) {
+    if (err) console.log('error finding a matching task');
+    else console.log('success finding a matching task');
+  });
+
+  res.status(200).send(sha1);
 
 });
 
